6fca474034dd183995a388197002ded651c8adb7
DasSpike <lgosspike@gmail.com>
2016-10-29 02:33:17 +0200

    removed unnecessary ui update calls

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ CELL_WIDTH = 2
 
 def main():
     def draw():
-        master.update_idletasks()
         master.update()
 
     master = Tk()
@@ -17,11 +16,8 @@ def main():
     label = Label(master, image=maze.get_image())
     label.grid()
 
-    master.update_idletasks()
     master.update()
-
     maze.create_maze()
-
     master.mainloop()
 
 if __name__ == "__main__":
